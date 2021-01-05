349fba49d324e9f19f9393de3a75860e718b7635
T0m2223 <64265856+T0m2223@users.noreply.github.com>
2021-01-05 12:40:11 +0100

    added game.litidata file, changed keytyped to keypressed for hotbar shortcuts, added files to gitignore

--- src/de/litigame/PlayerInput.java
+++ src/de/litigame/PlayerInput.java
@@ -7,16 +7,16 @@ import de.gurkenlabs.litiengine.input.Input;
 
 public final class PlayerInput {
 	public static void init() {
-		Input.keyboard().onKeyTyped(KeyEvent.VK_X, e -> Game.screens().display("ingame"));
-		Input.keyboard().onKeyTyped(KeyEvent.VK_1, e -> Hotbar.slot = 0);
-		Input.keyboard().onKeyTyped(KeyEvent.VK_2, e -> Hotbar.slot = 1);
-		Input.keyboard().onKeyTyped(KeyEvent.VK_3, e -> Hotbar.slot = 2);
-		Input.keyboard().onKeyTyped(KeyEvent.VK_4, e -> Hotbar.slot = 3);
-		Input.keyboard().onKeyTyped(KeyEvent.VK_5, e -> Hotbar.slot = 4);
-		Input.keyboard().onKeyTyped(KeyEvent.VK_6, e -> Hotbar.slot = 5);
-		Input.keyboard().onKeyTyped(KeyEvent.VK_7, e -> Hotbar.slot = 6);
-		Input.keyboard().onKeyTyped(KeyEvent.VK_8, e -> Hotbar.slot = 7);
-		Input.keyboard().onKeyTyped(KeyEvent.VK_9, e -> Hotbar.slot = 8);
+		Input.keyboard().onKeyPressed(KeyEvent.VK_X, e -> Game.screens().display("ingame"));
+		Input.keyboard().onKeyPressed(KeyEvent.VK_1, e -> Hotbar.slot = 0);
+		Input.keyboard().onKeyPressed(KeyEvent.VK_2, e -> Hotbar.slot = 1);
+		Input.keyboard().onKeyPressed(KeyEvent.VK_3, e -> Hotbar.slot = 2);
+		Input.keyboard().onKeyPressed(KeyEvent.VK_4, e -> Hotbar.slot = 3);
+		Input.keyboard().onKeyPressed(KeyEvent.VK_5, e -> Hotbar.slot = 4);
+		Input.keyboard().onKeyPressed(KeyEvent.VK_6, e -> Hotbar.slot = 5);
+		Input.keyboard().onKeyPressed(KeyEvent.VK_7, e -> Hotbar.slot = 6);
+		Input.keyboard().onKeyPressed(KeyEvent.VK_8, e -> Hotbar.slot = 7);
+		Input.keyboard().onKeyPressed(KeyEvent.VK_9, e -> Hotbar.slot = 8);
 		Input.mouse().onWheelMoved(e -> Hotbar.changeSlot(e));
 	}
 }
